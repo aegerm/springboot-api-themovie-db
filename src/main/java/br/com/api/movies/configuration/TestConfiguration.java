20be0af5e6eff46d50aa85efefda715af4557d3e
Alexandre Eger Marques <alexandreegermarques@gmail.com>
2020-05-24 22:31:37 -0300

    Implementado ambiente de testes.

---
+++ src/main/java/br/com/api/movies/configuration/TestConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.api.movies.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfiguration {
+}
